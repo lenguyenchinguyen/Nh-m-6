1a1c2f00c412fd6cc4ec06f8f68c5b07e210deba
lenguyenchinguyen <145195459+lenguyenchinguyen@users.noreply.github.com>
2023-11-28 15:45:23 +0700

    module sua ly code

---
+++ module/cart.php
@@ -0,0 +1,83 @@
+<?php
+class ServiceCart
+{
+    function insertBill($idUser, $name, $address, $tel, $email, $payment, $total)
+    {
+        $time = date('d/m/Y');
+        $sql = "INSERT INTO BILL VALUES(NULL, '$name', '$address', '$tel', '$email', $payment, '$time', $total, 0, $idUser)";
+        return pdo_execute_last_result($sql);
+    }
+    function insertCart($idUser, $img, $name, $price, $soluong, $idBill)
+    {
+        $sql = "INSERT CART VALUES(NULL, $idUser, '$img', '$name', $price, $soluong, $idBill)";
+        pdo_execute($sql);
+    }
+
+    function queryBill($id = null, $type = 'object')
+    {
+        if ($id && $type === 'object') {
+            $sql = "SELECT * FROM BILL WHERE ID = $id";
+            return pdo_query_one($sql);
+        } else if ($id && $type === 'array') {
+            $sql = "SELECT * FROM BILL WHERE ID = $id";
+            return pdo_query($sql);
+        } else {
+            $sql = "SELECT * FROM BILL";
+            return pdo_query($sql);
+        }
+    }
+
+    function queryCart($id)
+    {
+        $sql = "SELECT * FROM CART WHERE IDBILL = $id";
+        return pdo_query($sql);
+    }
+
+    function queryBills($idUser)
+    {
+        $sql = "SELECT * FROM BILL WHERE IDUSER = $idUser";
+        return pdo_query($sql);
+    }
+
+    function getStatusBill($status)
+    {
+        switch ($status) {
+            case 0:
+                return "Đơn hàng mới";
+            case 1:
+                return "Đang xử lý";
+            case 2:
+                return "Đang giao hàng";
+            case 3:
+                return "Hoàn tất";
+        }
+    }
+    function getAmountCart($idBill)
+    {
+        return sizeof($this->queryCart($idBill));
+    }
+
+    function deleteBill($id)
+    {
+        $sql = "DELETE FROM BILL WHERE ID = $id";
+        pdo_execute($sql);
+    }
+
+    function updateBill($id, $status)
+    {
+        $sql = "UPDATE BILL SET STATUS=$status WHERE ID = $id";
+        pdo_execute($sql);
+    }
+
+    function analytics()
+    {
+        $sql = "SELECT DANHMUC.NAME AS NAMEDM, COUNT(SANPHAM.ID) AS COUNTSP, MIN(SANPHAM.PRICE) AS MINPRICE, MAX(SANPHAM.PRICE) AS MAXPRICE, AVG(SANPHAM.PRICE) AS AVGPRICE FROM SANPHAM LEFT JOIN DANHMUC ON SANPHAM.IDDM = DANHMUC.ID GROUP BY DANHMUC.ID ORDER BY DANHMUC.ID DESC";
+        return pdo_query($sql);
+    }
+
+    function analyticsPrice($type = 'min')
+    {
+    }
+}
+
+$serviceCart = new ServiceCart();
