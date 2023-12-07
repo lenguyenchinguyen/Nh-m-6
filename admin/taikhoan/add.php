8e699fb3ab3ad0c33149e5b4314c407817eb8a31
lenguyenchinguyen <145195459+lenguyenchinguyen@users.noreply.github.com>
2023-12-07 16:43:28 +0700

    admin cap nhat

--- admin/taikhoan/add.php
+++ admin/taikhoan/add.php
@@ -1,29 +1,33 @@
-<h1>THÊM MỚI TÀI KHOẢN</h1>
+<center><h1>THÊM MỚI TÀI KHOẢN</h1></center>
 <form action="index.php?act=add-user" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
-    <div class="mb-3">
-        <label for="" class="form-label">ID khách hàng</label>
-        <input type="text" name="id" id="id" placeholder="Auto tăng" disabled class="form-control" aria-describedby="">
-    </div>
     <div class="mb-3">
         <label for="" class="form-label">User</label>
         <input type="text" class="form-control" name="user" id="user" placeholder="Nhập tên người dùng">
+        <span id="error_user" style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Password</label>
         <input type="password" class="form-control" name="pass" id="pass" placeholder="nhập password">
+        <span id="error_pass" style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Email</label>
         <input type="email" class="form-control" name="email" id="email" placeholder="Nhập email">
+        <span id="error_email" style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Address</label>
         <input type="text" class="form-control" name="address" id="address" placeholder="Nhập địa chỉ">
+        <span id="error_address" style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Tel</label>
         <input type="text" class="form-control" name="tel" id="tel" placeholder="Nhập vào SDT">
+        <span id="error_tel" style="color: red;"></span>
     </div>
+    <div id="error_message" style="color: red;"></div>
+
+
     <div class="mb-3">
         <label for="">Role</label>
         <div class="form-control">
@@ -32,57 +36,64 @@
         </div>
     </div>
 
-    <div id="error_message"></div>
-
     <button type="submit" name="btn-add" class="btn btn-primary">Submit</button>
     <button type="reset" class="btn btn-primary">Nhập lại</button>
-    <a href="index.php?act=list-user"><input class="btn btn-primary" type="button" value="DANH SÁCH"/></a>
+    <a href="index.php?act=list-user"><input class="btn btn-primary" type="button" value="DANH SÁCH" /></a>
 </form>
 
 <script>
     function validateForm() {
-    var user = document.getElementById("user").value; // Lấy giá trị trong trường "User"
-    var pass = document.getElementById("pass").value; // Lấy giá trị trong trường "Password"
-    var email = document.getElementById("email").value; // Lấy giá trị trong trường "Email"
-    var address = document.getElementById("address").value; // Lấy giá trị trong trường "Address"
-    var tel = document.getElementById("tel").value; // Lấy giá trị trong trường "Tel"
+        var user = document.getElementById("user").value;
+        var pass = document.getElementById("pass").value;
+        var email = document.getElementById("email").value;
+        var address = document.getElementById("address").value;
+        var tel = document.getElementById("tel").value;
 
-    // Kiểm tra nếu có trường nào bị bỏ trống, hiển thị thông báo và ngăn form submit
+        // Reset error messages
+        document.getElementById("error_user").innerHTML = "";
+        document.getElementById("error_pass").innerHTML = "";
+        document.getElementById("error_email").innerHTML = "";
+        document.getElementById("error_address").innerHTML = "";
+        document.getElementById("error_tel").innerHTML = "";
 
-    if (user.trim() === "") {
-        document.getElementById("error_message").innerHTML = "Vui lòng nhập họ tên";
-        return false; // Ngăn form submit
+        // Validate each field
+        var hasErrors = false;
+
+        if (user.trim() === "") {
+            document.getElementById("error_user").innerHTML = "Vui lòng nhập họ tên";
+            hasErrors = true;
+        }
+
+        if (pass.trim() === "") {
+            document.getElementById("error_pass").innerHTML = "Vui lòng nhập mật khẩu";
+            hasErrors = true;
+        }
+
+        if (email.trim() === "") {
+            document.getElementById("error_email").innerHTML = "Vui lòng nhập địa chỉ email";
+            hasErrors = true;
+        }
+
+        if (address.trim() === "") {
+            document.getElementById("error_address").innerHTML = "Vui lòng nhập địa chỉ";
+            hasErrors = true;
+        }
+
+        if (tel.trim() === "") {
+            document.getElementById("error_tel").innerHTML = "Vui lòng nhập số điện thoại";
+            hasErrors = true;
+        } else {
+            // Check if tel contains only numbers
+            var telRegex = /^\d+$/;
+            if (!telRegex.test(tel)) {
+                document.getElementById("error_tel").innerHTML = "Số điện thoại chỉ được nhập số";
+                hasErrors = true;
+            }
+        }
+        // Return true if there are no errors, false otherwise
+        return !hasErrors;
     }
-
-    if (pass.trim() === "") {
-        document.getElementById("error_message").innerHTML = "Vui lòng nhập mật khẩu";
-        return false; // Ngăn form submit
-    }
-
-    if (email.trim() === "") {
-        document.getElementById("error_message").innerHTML = "Vui lòng nhập địa chỉ email";
-        return false; // Ngăn form submit
-    }
-
-    if (address.trim() === ""){
-        document.getElementById("error_message").innerHTML = "Vui lòng nhập địa chỉ";
-        return false; // Ngăn form submit
-    }
-
-    if (tel.trim() === "") {
-        document.getElementById("error_message").innerHTML = "Vui lòng nhập số điện thoại";
-        return false; // Ngăn form submit
-    }
-
-    // Kiểm tra xem trường SĐT có chỉ chứa số hay không
-    var telRegex = /^\d+$/;
-    if (!telRegex.test(tel)) {
-        document.getElementById("error_message").innerHTML = "Số điện thoại chỉ được nhập số";
-        return false; // Ngăn form submit
-    }
-
-    // Nếu tất cả các trường đều được nhập đầy đủ và SĐT chỉ chứa số, cho phép form submit
-    return true;
-}
-
 </script>
+
+
+
--- admin/taikhoan/update.php
+++ admin/taikhoan/update.php
@@ -1,5 +1,5 @@
-<h1>THÊM MỚI SẢN PHẨM</h1>
-<form action="index.php?act=update-user" method="POST" enctype="multipart/form-data">
+<center><h1>CẬP NHẬT TÀI KHOẢN</h1></center>
+<form action="index.php?act=update-user" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
     <div class="mb-3">
         <label for="" class="form-label">ID khách hàng</label>
         <input type="text" name="id" placeholder="" disabled class="form-control" aria-describedby="">
@@ -7,23 +7,28 @@
     </div>
     <div class="mb-3">
         <label for="" class="form-label">User</label>
-        <input type="text" class="form-control" name="user" placeholder="Nhập vào tên" value="<?php echo $user['user'] ?>">
+        <input type="text" class="form-control" name="user" placeholder="Nhập vào tên" id="user" value="<?php echo $user['user'] ?>">
+        <span id="error_user" style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Password</label>
-        <input type="password" class="form-control" name="pass" placeholder="" value="<?php echo $user['pass'] ?>">
+        <input type="password" class="form-control" name="pass" placeholder="" id="pass" value="<?php echo $user['pass'] ?>">
+        <span id="error_pass"style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Email</label>
-        <input type="email" class="form-control" name="email" placeholder="Nhập email" value="<?php echo $user['email'] ?>">
+        <input type="email" class="form-control" name="email" placeholder="Nhập email" id="email" value="<?php echo $user['email'] ?>">
+        <span id="error_email"style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Address</label>
-        <input type="text" class="form-control" name="address" placeholder="Nhập địa chỉ" value="<?php echo $user['address'] ?>">
+        <input type="text" class="form-control" name="address" placeholder="Nhập địa chỉ" id="address" value="<?php echo $user['address'] ?>">
+        <span id="error_address"style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Tel</label>
-        <input type="text" class="form-control" name="tel" placeholder="Nhập vào sđt" value="<?php echo $user['tel'] ?>">
+        <input type="text" class="form-control" name="tel" placeholder="Nhập vào sđt" id="tel" value="<?php echo $user['tel'] ?>">
+        <span id="error_tel"style="color: red;"></span>
     </div>
     <div class="mb-3">
         <label for="">Role</label>
@@ -33,8 +38,62 @@
         </div>
     </div>
 
-
     <button type="submit" name="btn-update" class="btn btn-primary">Submit</button>
     <button type="reset" class="btn btn-primary">Nhập lại</button>
     <a href="index.php?act=list-account"><input class="btn btn-primary" type="button" value="DANH SÁCH" /></a>
 </form>
+
+<script>
+    function validateForm() {
+        var user = document.getElementById("user").value;
+        var pass = document.getElementById("pass").value;
+        var email = document.getElementById("email").value;
+        var address = document.getElementById("address").value;
+        var tel = document.getElementById("tel").value;
+
+        // Reset error messages
+        document.getElementById("error_user").innerHTML = "";
+        document.getElementById("error_pass").innerHTML = "";
+        document.getElementById("error_email").innerHTML = "";
+        document.getElementById("error_address").innerHTML = "";
+        document.getElementById("error_tel").innerHTML = "";
+
+        // Validate each field
+        var hasErrors = false;
+
+        if (user.trim() === "") {
+            document.getElementById("error_user").innerHTML = "Vui lòng nhập họ tên";
+            hasErrors = true;
+        }
+
+        if (pass.trim() === "") {
+            document.getElementById("error_pass").innerHTML = "Vui lòng nhập mật khẩu";
+            hasErrors = true;
+        }
+
+        if (email.trim() === "") {
+            document.getElementById("error_email").innerHTML = "Vui lòng nhập địa chỉ email";
+            hasErrors = true;
+        }
+
+        if (address.trim() === "") {
+            document.getElementById("error_address").innerHTML = "Vui lòng nhập địa chỉ";
+            hasErrors = true;
+        }
+
+        if (tel.trim() === "") {
+            document.getElementById("error_tel").innerHTML = "Vui lòngnhập số điện thoại";
+            hasErrors = true;
+        } else {
+            // Check if tel contains only numbers
+            var telRegex = /^\d+$/;
+            if (!telRegex.test(tel)) {
+                document.getElementById("error_tel").innerHTML = "Số điện thoại chỉ được nhập số";
+                hasErrors = true;
+            }
+        }
+
+        // Return true if there are no errors, false otherwise
+        return !hasErrors;
+    }
+</script>
